9c7568969299220554eae723399415c989e9c182
Trevor Wayman <way16006@byui.edu>
2022-05-14 20:56:08 -0600

    Week 4 in class updates, redis and md5 hash changes.

--- server.js
+++ server.js
@@ -2,7 +2,17 @@ const express = require('express');//Import express library
 const bodyParser = require('body-parser'); //call body parser middleware
 const app = express();//Use express
 const port = 3000;
+const redis = require('redis');
 
+const redisClient = redis.createdClient('hget passwords');
+
+// const md5 = require('md5');
+//const hashedPasswordfromuser = md5(req.body.password);
+/* hset passwords scmurdock@gmail.com P@ssW0rd
+hget passwords scmurdock@gmail.com hash
+hset myhash password //password; //set hash to the hash of the password given
+
+*/
 
 
 app.use(bodyParser.json());//use middleware
@@ -11,9 +21,11 @@ app.listen(port,()=>{console.log("I am listening! "+port)});
 
 
 app.post('/login',(request,response)=>{
+    const hashedPassword = md5(req.body.password);
+    const passwordHashRedis = redisClient.hGet('password');
     const loginRequest = request.body;
     //console.log(JSON.stringify(request.body));
-    if (loginRequest.userName=='scmurdock@gmail.com' && loginRequest.password=='P@ssW0rd')
+    if (loginRequest.userName=='scmurdock@gmail.com' && loginRequest.password=='5ca33d221fd09f16c1ecba9c1aadc3eb')
     {
         response.status(200);
         response.send('Welcome');
@@ -23,6 +35,7 @@ app.post('/login',(request,response)=>{
     }
 });
 
+const validatePass = (request, respose)=>{}
 app.get('/',(request,response)=>{
     response.send("Hello")
 }); //Response
